cf2f79f2346ece7a593477da24ecb13f1f3d25bd
Emanuel Souza De Quadros <ems24@bid-ems24-deb>
2024-06-13 13:45:34 +0200

    add config.literature_folder

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ config.read('config.ini')
 brenda_json = config['data']['brenda_json']
 references_file = config['data']['references_file']
 species_list = config['data']['species_list']
+literature_folder = config['data']['literature_folder']
 esummaries = config['data']['esummaries']
 
 entrez_email = os.environ.get('ENTREZ_EMAIL')
