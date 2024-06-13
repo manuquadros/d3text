e4c4567f02a346107dedbd1f70c7e88b8be53d81
Emanuel Souza De Quadros <ems24@bid-ems24-deb>
2024-06-13 13:44:42 +0200

    add get_pmcs.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -6,3 +7,6 @@ config.read('config.ini')
 brenda_json = config['data']['brenda_json']
 references_file = config['data']['references_file']
 species_list = config['data']['species_list']
+esummaries = config['data']['esummaries']
+
+entrez_email = os.environ.get('ENTREZ_EMAIL')
