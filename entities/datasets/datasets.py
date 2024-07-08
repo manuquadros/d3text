687b789aeea83f906f064f69c2e1a7021a0f79c9
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-07-08 08:07:30 +0200

    feat: new species800 interface, with splits

--- entities/datasets/datasets.py
+++ entities/datasets/datasets.py
@@ -1,5 +1,10 @@
 import s800classed
+import datasets
 
 
-def species800():
-    return s800classed.load()
+def species800(
+        train_size: float = 0.15,
+        dev_size: float = 0.15,
+        seed: int = 4
+) -> datasets.Dataset:
+    return s800classed.load(train_size, dev_size, seed)
