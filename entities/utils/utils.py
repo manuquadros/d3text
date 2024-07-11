710dae5e10401e3a831f954a41d8e193bd80e378
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-07-11 16:22:06 +0200

    refactor: utils.py

---
+++ entities/utils/utils.py
@@ -0,0 +1,81 @@
+import collections
+import csv
+import dataclasses
+import functools
+import os
+from collections.abc import Iterable, Iterator
+
+import datasets
+
+
+def merge_tokens(
+    tokens: Iterable, tags: Iterator, true_tags: Iterable
+) -> dict[str, list[str]]:
+    merged_tokens: list[str] = []
+    merged_labels: list[str] = []
+    merged_true: list[str] = []
+    tokens = iter(tokens)
+    tags = iter(tags)
+    true_tags = iter(true_tags)
+
+    cur_token, cur_pred, cur_true = next(tokens), next(tags), next(true_tags)
+
+    while cur_token != "[SEP]":
+        if cur_token == "[CLS]":
+            cur_token, cur_pred, cur_true = next(tokens), next(tags), next(true_tags)
+
+        if cur_token.startswith("##"):
+            merged_tokens[-1] += cur_token[2:]
+        else:
+            merged_tokens.append(cur_token)
+            merged_labels.append(cur_pred)
+            merged_true.append(cur_true)
+
+        cur_token, cur_pred, cur_true = (
+            next(tokens),
+            next(tags),
+            next(true_tags),
+        )
+
+    return {"tokens": merged_tokens, "predicted": merged_labels, "true": merged_true}
+
+
+def upsample(data: datasets.Dataset, label: str) -> datasets.Dataset:
+    print("Upsampling...")
+    if not label.startswith("B-"):
+        label = "B-" + label
+
+    counter = functools.reduce(
+        lambda a, b: a + b, (entity_counter(seq) for seq in data["nerc_tags"])
+    )
+
+    target = counter.most_common(1)[0][1] - counter[label]
+    label_seqs = data.filter(lambda s: label in s["nerc_tags"])
+    label_seqs = label_seqs.filter(
+        lambda s: entity_counter(s["nerc_tags"]).most_common(1)[0][0] == label
+    )
+
+    new_samples: list[datasets.Dataset] = []
+
+    while len(new_samples) < target:
+        new_samples.append(label_seqs.shuffle().take(1))
+
+    print(f"Adding {len(new_samples)} samples.")
+
+    return datasets.concatenate_datasets(new_samples + [data])
+
+
+def entity_counter(sequence: list[str]) -> collections.Counter:
+    return collections.Counter(label for label in sequence if label.startswith("B"))
+
+
+def log_model(filename, config, val_loss) -> None:
+    config = dataclasses.asdict(config)
+    config["val_loss"] = val_loss
+    newfile = not os.path.exists(filename) or os.stat(filename).st_size == 0
+
+    with open("models.csv", "a", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=config.keys())
+        if newfile:
+            writer.writeheader()
+        writer.writerow(config)
