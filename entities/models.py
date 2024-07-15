87f24662ba49a1834b606d393cce06107a48872f
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-07-15 10:00:07 +0200

    build: utils

--- entities/models.py
+++ entities/models.py
@@ -8,11 +8,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import transformers
+from entities import utils
 from seqeval.metrics import classification_report
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-
-import utils
 
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
