ce5ab305ea8402dac9cf0ce3f80814ae0c043b78
Emanuel Quadros <45699727+manuquadros@users.noreply.github.com>
2024-07-15 10:01:34 +0200

    refactor: move tuning.py to the root of the package

--- tuning.py
+++ tuning.py
@@ -1,16 +1,21 @@
-import data
-import models
 import random
+
 import torch._dynamo
+
+from entities import data, models
+
 torch.set_float32_matmul_precision("high")
 
+import os
+
+os.environ["TRANSFORMERS_OFFLINE"] = "1"
 
 ds = data.load_dataset(data.only_species_and_strains800())
 
 for _ in range(3):
     configs = list(models.model_configs())
     random.shuffle(configs)
-    
+
     for config in configs:
         nt = models.NERCTagger(ds, config=config)
         nt.cuda()
