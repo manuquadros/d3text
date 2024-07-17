0d1763c2a676f80fd3092327fa043f3442909c0b
Emanuel Quadros <45699727+manuquadros@users.noreply.github.com>
2024-07-17 09:47:49 +0200

    fix: adapt to DatasetConfig format

--- entities/models.py
+++ entities/models.py
@@ -89,7 +89,7 @@ class Model(torch.nn.Module):
             batch_losses = []
 
             print(f"Epoch {epoch + 1}")
-            for i, batch in tqdm(enumerate(training_data)):
+            for i, batch in tqdm(enumerate(train_data.data)):
                 inputs, labels = batch["sequence"], batch["nerc_tags"].to(self.device)
                 inputs = {k: v.to(self.device) for k, v in inputs.items()}
 
