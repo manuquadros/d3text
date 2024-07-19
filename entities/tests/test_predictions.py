05522c5ba7f1dd7464dbf4aa05e9209735a8646a
Emanuel Quadros <45699727+manuquadros@users.noreply.github.com>
2024-07-19 17:00:17 +0200

    feat: merge_tokens now works with or without gold labels

---
+++ entities/tests/test_predictions.py
@@ -0,0 +1,85 @@
+from entities import utils
+
+
+def test_merging_without_gold_standard():
+    tokens = (
+        "[CLS]",
+        "genetic",
+        "analysis",
+        "of",
+        "the",
+        "xenobiotic",
+        "resistance",
+        "-",
+        "associated",
+        "abc",
+        "gene",
+        "subfamilies",
+        "of",
+        "the",
+        "lepid",
+        "##optera",
+        ".",
+        "[SEP]",
+    )
+
+    tags = (
+        "#",
+        "O",
+        "O",
+        "O",
+        "O",
+        "O",
+        "O",
+        "O",
+        "O",
+        "O",
+        "O",
+        "O",
+        "O",
+        "O",
+        "B-OOS",
+        "B-OOS",
+        "O",
+        "#",
+    )
+
+    merged = {
+        "tokens": [
+            "genetic",
+            "analysis",
+            "of",
+            "the",
+            "xenobiotic",
+            "resistance",
+            "-",
+            "associated",
+            "abc",
+            "gene",
+            "subfamilies",
+            "of",
+            "the",
+            "lepidoptera",
+            ".",
+        ],
+        "predicted": [
+            "O",
+            "O",
+            "O",
+            "O",
+            "O",
+            "O",
+            "O",
+            "O",
+            "O",
+            "O",
+            "O",
+            "O",
+            "O",
+            "B-OOS",
+            "O",
+        ],
+    }
+
+    
+    assert utils.merge_tokens(tokens, tags) == merged
