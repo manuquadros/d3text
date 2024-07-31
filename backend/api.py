dc03664f1cd1ed012ba3832ef221e00f95ed2e04
Emanuel Quadros <45699727+manuquadros@users.noreply.github.com>
2024-07-31 11:01:07 +0200

    fix: allow connections from the frontend

--- backend/api.py
+++ backend/api.py
@@ -1,12 +1,15 @@
 from db import Annotation, Annotator, SQLModel, Text, engine
 from fastapi import FastAPI
 from sqlmodel import Session, select
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["http://localhost:5173"]
 
 @app.get("/annotators/")
 def read_annotators():
     with Session(engine) as session:
         annotators = session.exec(select(Annotator)).all()
         return annotators
+app.add_middleware(CORSMiddleware, allow_origins=origins)
