e217190ba05b21cc43e318fab86c8c5a7b0f0be7
Emanuel Quadros <45699727+manuquadros@users.noreply.github.com>
2024-07-31 11:03:12 +0200

    feat: main segment retrieval function

--- backend/api.py
+++ backend/api.py
@@ -1,15 +1,30 @@
-from db import Annotation, Annotator, SQLModel, Text, engine
+from datamodel import Response
+from db import db_init, query_article, query_chunk, random_chunk
 from fastapi import FastAPI
-from sqlmodel import Session, select
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 origins = ["http://localhost:5173"]
 
-@app.get("/annotators/")
-def read_annotators():
-    with Session(engine) as session:
-        annotators = session.exec(select(Annotator)).all()
-        return annotators
 app.add_middleware(CORSMiddleware, allow_origins=origins)
+
+
+@app.on_event("startup")
+def on_startup():
+    db_init()
+
+
+@app.get("/segment/")
+def show_segment(pmid: int = 0, start: int = 0) -> str:
+    if pmid and start:
+        print(f"Getting chunk {start} from PMID {pmid}")
+        segment = query_chunk(pmid, start)
+    elif pmid:
+        print(f"Retrieving article {pmid}")
+        segment = query_article(pmid)
+    else:
+        print("Getting random chunk")
+        segment = random_chunk()
+
+    return segment.model_dump_json()
