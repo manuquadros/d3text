0804d1eabbfb3102b91722d2d54e471ba9f04e0a
Emanuel Quadros <45699727+manuquadros@users.noreply.github.com>
2024-08-05 12:21:10 +0200

    refactor: add overloaded query function, calling transform_article only once

--- backend/db.py
+++ backend/db.py
@@ -3,10 +3,11 @@ import os
 from datamodel import (Annotation, Annotator, Response, SQLModel, Text,
                        TextChunk)
 from log import logger
+from multimethod import multimethod
 from sqlalchemy.sql.functions import random
 from sqlmodel import Session, create_engine, select
 
-from xmlparser import get_chunk, transform_article
+from xmlparser import get_chunk
 
 db_path = os.path.join(os.path.dirname(__file__), "database.db")
 
@@ -21,7 +22,35 @@ def db_init() -> None:
     SQLModel.metadata.create_all(engine)
 
 
-def random_chunk() -> Response:
+@multimethod
+def query(pmid: int, start: int) -> Response:
+    with Session(engine) as session:
+        chunk, article = next(
+            session.exec(
+                select(TextChunk, Text)
+                .join(Text)
+                .where(Text.pmid == pmid)
+                .where(TextChunk.start == start)
+            )
+        )
+
+    return Response(
+        article=article,
+        chunk=chunk,
+        content=get_chunk(article.content, chunk.start, chunk.stop),
+    )
+
+
+@query.register
+def _(pmid: int) -> Text:
+    with Session(engine) as session:
+        article = next(session.exec(select(Text).where(Text.pmid == pmid)))
+
+    return article
+
+
+@query.register
+def _() -> Response:
     with Session(engine) as session:
         chunk = next(
             session.exec(select(TextChunk).order_by(random()).limit(1))
@@ -35,34 +64,3 @@ def random_chunk() -> Response:
         chunk=chunk,
         content=get_chunk(article.content, chunk.start, chunk.stop),
     )
-
-
-def query_chunk(pmid: int, start: int) -> Response:
-    with Session(engine) as session:
-        chunk, article = next(
-            session.exec(
-                select(TextChunk, Text)
-                .join(Text)
-                .where(Text.pmid == pmid)
-                .where(TextChunk.start == start)
-            )
-        )
-
-    content = get_chunk(article.content, chunk.start, chunk.stop)
-    logger.debug(content)
-    content = transform_article(content)
-    logger.debug(content)
-
-    return Response(
-        article=article,
-        chunk=chunk,
-        content=content,
-    )
-
-
-def query_article(pmid: int) -> Text:
-    with Session(engine) as session:
-        article = next(session.exec(select(Text).where(Text.pmid == pmid)))
-
-    article.content = transform_article(article.content)
-    return article
