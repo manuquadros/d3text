78d8cf6972b6a8c6060e64b7694e8e8f21166bfb
Emanuel Quadros <45699727+manuquadros@users.noreply.github.com>
2024-08-21 14:29:28 +0200

    feat: add script to annotate db articles

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,31 @@
 from typing import Optional
 
+from datamodel import HtmlArticle, Response
+from pydantic import EmailStr
+from tokenizers.normalizers import BertNormalizer
+
+from xmlparser import split_metadata_body
+
+
+def get_batch(annotator_email: EmailStr, batch_size: int) -> list[HtmlArticle]:
+    chunks = []
+    for item in db.unannotated(annotator_email, batch_size):
+        chunks.append(response_to_article(item))
+
+    return chunks
+
+
+def response_to_article(item: Response) -> HtmlArticle:
+    content = BertNormalizer(lowercase=False).normalize_str(item.content)
+    metadata, body = split_metadata_body(content)
+
+    return HtmlArticle(
+        article_id=item.article.id,
+        chunk_id=item.chunk.id,
+        metadata=metadata,
+        body=body,
+    )
+
 
 def safe_concat(string: Optional[str], suffix: Optional[str]) -> str | None:
     match (string, suffix):
