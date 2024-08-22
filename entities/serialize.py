ecdccc5993e65a1cc05235ad312880ed14a932c2
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-08-22 16:40:08 +0200

    fix: impose some limits on the allowance for discontinuities (to be revised)

--- entities/serialize.py
+++ entities/serialize.py
@@ -10,21 +10,31 @@ def serialize_triples(tokens: Sequence[Token], source: str) -> str:
 
     tokens = merge_off_tokens(tokens)
 
-    entity_counter = 1
+    entity_counter = 0
+    last_entity_type = ""
+    gap = 0
     offset = 0
 
     for token in tokens:
         space = " " * (token.offset[0] - offset)
         if token.prediction in ("#", "O"):
             output += space + token.string
+            gap += 1
         elif token.prediction.startswith("B-"):
-            output += space + entity_string(token=token, ent_id=entity_counter)
             entity_counter += 1
+            gap = 0
+            output += space + entity_string(token=token, ent_id=entity_counter)
+            last_entity_type = token.prediction[2:]
         elif output.endswith("</span>"):
             output = output[:-7]
             output += f"{space}{token.string}</span>"
         else:
-            # Here there is a discontinuity. Just use the last id.
+            # Here there is a discontinuity. Just use the last id if the type matches
+            # and if the last entity is not too far.
+            if not (token.prediction[2:] == last_entity_type and gap <= 3):
+                last_entity_type = token.prediction[2:]
+                gap = 0
+                entity_counter += 1
             output += entity_string(token=token, ent_id=entity_counter)
 
         offset = token.offset[1]
