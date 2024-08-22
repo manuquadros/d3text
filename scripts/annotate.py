9733f9a1e6684f55cdb6baecdb88ce344c3d76e1
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-08-22 11:59:14 +0200

    chore: cleanup

--- scripts/annotate.py
+++ scripts/annotate.py
@@ -3,16 +3,16 @@
 import os
 from itertools import starmap
 
-import db
 import torch
 import torch._dynamo
 from datamodel import Annotation
+from db import (get_annotator, get_batch, query, response_to_article,
+                save_annotations)
 
 from config import load_model_config
 from entities.models import NERCTagger
 from entities.serialize import serialize_triples
-from utils import get_batch
-from xmlparser import reinsert_tags, remove_tags, split_metadata_body
+from xmlparser import reinsert_tags, remove_tags
 
 os.environ["TRANSFORMERS_OFFLINE"] = "1"
 
@@ -28,18 +28,17 @@ def main() -> None:
 
     print("Model loaded")
 
-    annotator = db.get_annotator("strain_annotator@dsmz.de", "Strain Annotator")
+    annotator = get_annotator("strain_annotator@dsmz.de", "Strain Annotator")
     batch = get_batch(annotator.email, config.batch_size)
+    # batch = [response_to_article(query(11914155))]
 
     while batch:
         batch_bodies = [sample.body for sample in batch]
         stripped = [remove_tags(body) for body in batch_bodies]
-        print("\n---\n".join(stripped))
         predictions = model.predict(stripped)
         serialized = starmap(serialize_triples, zip(predictions, batch_bodies))
-        retagged = starmap(reinsert_tags, zip(serialized, batch_bodies))
 
-        print(list(retagged))
+        retagged = starmap(reinsert_tags, zip(serialized, batch_bodies))
 
         annotations = (
             Annotation(
@@ -53,7 +52,7 @@ def main() -> None:
             for article, annotated in zip(batch, retagged)
         )
 
-        db.save_annotations(annotations)
+        save_annotations(annotations)
 
         break
 
