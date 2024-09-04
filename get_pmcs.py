550f20b3f7871a46c3961ca19ccce08e5937c328
Emanuel Quadros <45699727+manuquadros@users.noreply.github.com>
2024-09-04 14:47:59 +0200

    style: formatting

--- get_pmcs.py
+++ get_pmcs.py
@@ -1,14 +1,14 @@
-import os
 import json
 import logging
-import pandas as pd
+import os
 import urllib
 import xml.etree.ElementTree as ET
-import xmltodict
 from collections.abc import Iterable
-from tqdm import tqdm
 
+import pandas as pd
+import xmltodict
 from Bio import Entrez
+from tqdm import tqdm
 
 import config
 
@@ -16,13 +16,16 @@ Entrez.email = config.entrez_email
 
 references = pd.read_csv(config.references_file)
 
-logging.basicConfig(filename='debug.log', encoding='utf-8',
-                    level=logging.DEBUG,
-                    format=' %(name)s :: %(levelname)s :: %(message)s')
-logger = logging.getLogger('brenda')
+logging.basicConfig(
+    filename="debug.log",
+    encoding="utf-8",
+    level=logging.DEBUG,
+    format=" %(name)s :: %(levelname)s :: %(message)s",
+)
+logger = logging.getLogger("brenda")
 
 try:
-    with open(config.esummaries, 'r') as summaries:
+    with open(config.esummaries, "r") as summaries:
         esummaries = json.load(summaries)
 except FileNotFoundError:
     esummaries = {}
@@ -35,31 +38,33 @@ def format_fields(fields: list[dict] | dict) -> dict:
     The function eliminates repeated '@Name' keys and '@Type' annotations.
     """
     if isinstance(fields, dict):
-        return {fields['@Name']: fields.get('#text', '')}
+        return {fields["@Name"]: fields.get("#text", "")}
     else:
-        return {field['@Name']: format_fields(field['Item'])
-                if 'Item' in field
-                else field.get('#text', '')
-                for field in fields}
+        return {
+            field["@Name"]: format_fields(field["Item"])
+            if "Item" in field
+            else field.get("#text", "")
+            for field in fields
+        }
 
 
 def parse_records(records: ET.Element) -> dict:
-    """ Collect all the article summaries, using their PubMed IDs as dictionary keys. """    
-    records = xmltodict.parse(ET.tostring(records, encoding='utf-8'))
-    
+    """Collect all the article summaries, using their PubMed IDs as dictionary keys."""
+    records = xmltodict.parse(ET.tostring(records, encoding="utf-8"))
+
     try:
-        if 'ERROR' in records['eSummaryResult']:
-            logger.info(records['eSummaryResult']['ERROR'])
-        records = records['eSummaryResult']['DocSum']
+        if "ERROR" in records["eSummaryResult"]:
+            logger.info(records["eSummaryResult"]["ERROR"])
+        records = records["eSummaryResult"]["DocSum"]
     except KeyError:
         return {}
     else:
         if isinstance(records, list):
-            return {rec['Id']: format_fields(rec['Item']) for rec in records}
+            return {rec["Id"]: format_fields(rec["Item"]) for rec in records}
         else:
-            return {records['Id']: format_fields(records['Item'])}
+            return {records["Id"]: format_fields(records["Item"])}
 
-        
+
 def get_pmc(pubmed_id: str) -> str:
     """
     Retrieve PMC ID for a given PubMed ID.
@@ -67,13 +72,13 @@ def get_pmc(pubmed_id: str) -> str:
     If the PubMed ID is not found in `config.esummaries`, the function will try to retrieve
     the article summary from Entrez, as long as `retrieve` is set to `True`.
     """
-    if pubmed_id != '-':
+    if pubmed_id != "-":
         try:
-            pmc = esummaries[pubmed_id]['ArticleIds']['pmc']
-            return pmc.replace('PMC', '')
+            pmc = esummaries[pubmed_id]["ArticleIds"]["pmc"]
+            return pmc.replace("PMC", "")
         except KeyError:
             logger.info(f"{pubmed_id} does not have a corresponding PMC")
-    return ''
+    return ""
 
 
 def check_ids(references: pd.DataFrame) -> pd.DataFrame:
@@ -83,25 +88,33 @@ def check_ids(references: pd.DataFrame) -> pd.DataFrame:
     For IDs that are not already in `config.esummaries`, the function will retrieve the
     summaries from Entrez and update `config.esummaries`.
     """
-    
-    ids_to_retrieve = set([pid for pid in references['pubmed_id']
-                           if pid not in esummaries and pid != '-'])
+
+    ids_to_retrieve = set(
+        [
+            pid
+            for pid in references["pubmed_id"]
+            if pid not in esummaries and pid != "-"
+        ]
+    )
 
     if ids_to_retrieve:
         print(f"Retrieving {len(ids_to_retrieve)} from Entrez.")
 
-        with Entrez.esummary(db="pubmed", id=ids_to_retrieve) as summariesHandle:
+        with Entrez.esummary(
+            db="pubmed", id=ids_to_retrieve
+        ) as summariesHandle:
             records = parse_records(ET.parse(summariesHandle).getroot())
             if not records:
-                print("The IDs were not retrievable. They will be removed and save in "
-                      "unavailable_pubmed_ids.csv")
-                unavailable = references.query('pubmed_id in @ids_to_retrieve')
-                unavailable.to_csv('unavailable_pubmed_ids.csv', index=False)
-                
-                return references.query('pubmed_id not in @ids_to_retrieve')
-                
+                print(
+                    "The IDs were not retrievable. They will be removed and save in "
+                    "unavailable_pubmed_ids.csv"
+                )
+                unavailable = references.query("pubmed_id in @ids_to_retrieve")
+                unavailable.to_csv("unavailable_pubmed_ids.csv", index=False)
 
-        with open(config.esummaries, 'w') as esummaries_file:
+                return references.query("pubmed_id not in @ids_to_retrieve")
+
+        with open(config.esummaries, "w") as esummaries_file:
             esummaries.update(records)
             json.dump(esummaries, esummaries_file, indent=1)
     else:
@@ -110,13 +123,15 @@ def check_ids(references: pd.DataFrame) -> pd.DataFrame:
     return references
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Checking how many ids have to be retrieved from Entrez...")
     references = check_ids(references)
 
     tqdm.pandas(desc="Progress")
-    references['pmc'] = references['pubmed_id'].progress_apply(get_pmc)
+    references["pmc"] = references["pubmed_id"].progress_apply(get_pmc)
     references.to_csv(config.references_file, index=False)
 
-    print(f"{len([v for v in esummaries.values() if 'pmc' in v['ArticleIds']])}"
-          " articles with PMC IDs.")
+    print(
+        f"{len([v for v in esummaries.values() if 'pmc' in v['ArticleIds']])}"
+        " articles with PMC IDs."
+    )
--- get_reference_list.py
+++ get_reference_list.py
@@ -1,14 +1,13 @@
 import os
 import re
-import requests
 
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 import config
 import enzymes
-
 
 with open(config.species_list) as species_list:
     bacteria = set(s.strip() for s in species_list.readlines())
@@ -16,26 +15,30 @@ with open(config.species_list) as species_list:
 for enzyme in tqdm(enzymes.enzymes):
     references = []
 
-    url = f'https://www.brenda-enzymes.org/enzyme.php?ecno={enzyme}&onlyTable=Reference'
+    url = f"https://www.brenda-enzymes.org/enzyme.php?ecno={enzyme}&onlyTable=Reference"
 
-    references_html = BeautifulSoup(requests.get(url).content, features='html5lib')
+    references_html = BeautifulSoup(
+        requests.get(url).content, features="html5lib"
+    )
 
-    for row in references_html.find_all(id=re.compile('tab30r\d+sr0$')):
-        fields = row.find_all(id=re.compile('tab30r\d+sr0c\d+$'))
+    for row in references_html.find_all(id=re.compile("tab30r\d+sr0$")):
+        fields = row.find_all(id=re.compile("tab30r\d+sr0c\d+$"))
 
-        for organism in fields[7].get_text().split(','):
+        for organism in fields[7].get_text().split(","):
             organism = organism.strip()
             if organism in bacteria:
-                references.append({
-                    'enzyme': enzyme,
-                    'organism': organism,
-                    'reference': fields[0].get_text().strip(),
-                    'pubmed_id': fields[8].get_text().strip()
-                })
+                references.append(
+                    {
+                        "enzyme": enzyme,
+                        "organism": organism,
+                        "reference": fields[0].get_text().strip(),
+                        "pubmed_id": fields[8].get_text().strip(),
+                    }
+                )
 
     references = pd.DataFrame(references)
     references.to_csv(
         config.references_file,
-        mode='a',
-        header = not os.path.exists(config.references_file)
+        mode="a",
+        header=not os.path.exists(config.references_file),
     )
