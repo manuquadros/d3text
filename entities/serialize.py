ef03d85df6fcb68c82a88da317019038091a510a
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-09-09 16:46:45 +0200

    feat: combine resource names when tokens have the same string

--- entities/serialize.py
+++ entities/serialize.py
@@ -11,7 +11,9 @@ def serialize_triples(tokens: Sequence[Token]) -> str:
     tokens = merge_off_tokens(tokens)
 
     entity_counter = 0
+    entity_map: dict[tuple[str, str], str] = {}
     last_entity_type = ""
+    current_res = ""
     gap = 0
     offset = 0
 
@@ -21,9 +23,18 @@ def serialize_triples(tokens: Sequence[Token]) -> str:
             output += space + token.string
             gap += 1
         elif token.prediction.startswith("B-"):
-            entity_counter += 1
             gap = 0
-            output += space + entity_string(token=token, ent_id=entity_counter)
+            current_res = entity_map.get((token.string, token.prediction), "")
+            if current_res:
+                output += space + entity_string(token=token, ent_id=current_res)
+            else:
+                entity_counter += 1
+                entity_map[(token.string, token.prediction)] = (
+                    f"#T{entity_counter}"
+                )
+                output += space + entity_string(
+                    token=token, ent_id=entity_counter
+                )
             last_entity_type = token.prediction[2:]
         elif output.endswith("</span>"):
             output = output[:-7]
@@ -31,11 +42,19 @@ def serialize_triples(tokens: Sequence[Token]) -> str:
         else:
             # Here there is a discontinuity. Just use the last id if the type matches
             # and if the last entity is not too far.
-            if not (token.prediction[2:] == last_entity_type and gap <= 3):
+            if (
+                current_res
+                and token.prediction[2:] == last_entity_type
+                and gap <= 3
+            ):
+                output += space + entity_string(token=token, ent_id=current_res)
+            else:
                 last_entity_type = token.prediction[2:]
-                gap = 0
                 entity_counter += 1
-            output += space + entity_string(token=token, ent_id=entity_counter)
+                output += space + entity_string(
+                    token=token, ent_id=entity_counter
+                )
+            gap = 0
 
         offset = token.offset[1]
 
@@ -44,8 +63,11 @@ def serialize_triples(tokens: Sequence[Token]) -> str:
     return output
 
 
-def entity_string(token: Token, ent_id: int) -> str:
+def entity_string(token: Token, ent_id: str | int) -> str:
+    if isinstance(ent_id, int):
+        ent_id = f"#T{ent_id}"
     return (
-        f'<span class="entity" resource="#T{ent_id}" typeof="ncbitaxon:{token.prediction[2:]}">'
+        f'<span class="entity" resource="{ent_id}" '
+        f'typeof="ncbitaxon:{token.prediction[2:]}">'
         f"{token.string}</span>"
     )
