12ab0021b00fb74c36f5b536b52b87a35b34e862
Emanuel Quadros <45699727+manuquadros@users.noreply.github.com>
2025-05-13 13:21:16 +0200

    feat: export DatasetConfig from d3text.data

--- src/d3text/data/__init__.py
+++ src/d3text/data/__init__.py
@@ -1,3 +1,13 @@
-from .data import index_tensor, multi_hot_encode_series, brenda_dataset
+from .data import (
+    index_tensor,
+    multi_hot_encode_series,
+    brenda_dataset,
+    DatasetConfig,
+)
 
-__all__ = ["index_tensor", "multi_hot_encode_series", "brenda_dataset"]
+__all__ = [
+    "index_tensor",
+    "multi_hot_encode_series",
+    "brenda_dataset",
+    "DatasetConfig",
+]
