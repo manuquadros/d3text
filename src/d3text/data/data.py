ba100ecd26c59ceac88b6b17f462faee59547bc0
Emanuel Quadros <45699727+manuquadros@users.noreply.github.com>
2025-06-06 11:47:00 +0200

    refactor: compute loss inside compute_batch
    This is more practical now because we need more batch information in order to
    compute loss on the relations, and we don't want to duplicate this logic in
    train_model, validate_model, and evaluate_model

--- src/d3text/data/data.py
+++ src/d3text/data/data.py
@@ -28,6 +28,16 @@ os.environ["TOKENIZERS_PARALLELISM"] = "true"
 DATA_DIR = pathlib.Path(__file__).parent.parent.parent.parent / "data"
 
 
+# def seed_worker(worker_id):
+#     worker_seed = torch.initial_seed() % 2**32
+#     numpy.random.seed(worker_seed)
+#     random.seed(worker_seed)
+
+
+# g = torch.Generator()
+# g.manual_seed(4)
+
+
 @dataclasses.dataclass
 class DatasetConfig:
     data: datasets.DatasetDict | DataLoader | Dataset | dict[str, Dataset]
@@ -92,7 +102,12 @@ def get_batch_loader(
         batch_size=batch_size,
         drop_last=False,
     )
-    return DataLoader(dataset=dataset, sampler=sampler, pin_memory=False)
+    return DataLoader(
+        dataset=dataset,
+        sampler=sampler,
+        pin_memory=False,
+        #        worker_init_fn=seed_worker,
+    )
 
 
 def get_loader(
