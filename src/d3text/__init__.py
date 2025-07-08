165ecfba97c2134001a731532af6c94a83966631
Emanuel Quadros <45699727+manuquadros@users.noreply.github.com>
2025-07-08 12:39:25 +0200

    feat: check if stackprinter and beartype are installed before loading

--- src/d3text/__init__.py
+++ src/d3text/__init__.py
@@ -1,3 +1,15 @@
-from beartype.claw import beartype_this_package
+try:
+    import stackprinter
 
-beartype_this_package()
+    stackprinter.set_excepthook(style="darkbg2")
+except ModuleNotFoundError:
+    print(
+        "pip install stackprinter if you want stackprinter's exception messages."
+    )
+
+try:
+    from beartype.claw import beartype_this_package
+
+    beartype_this_package()
+except ModuleNotFoundError:
+    print("pip install beartype if you want runtime type-checking.")
