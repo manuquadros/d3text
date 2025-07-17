d0765aa7aa2f1c7c0d980f1d6dd1d96298e085f5
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2025-07-17 11:02:45 +0200

    refactor(dead code): remove keep_only

--- src/d3text/data/data.py
+++ src/d3text/data/data.py
@@ -5,7 +5,6 @@ import math
 import os
 import pathlib
 import random
-import re
 from collections.abc import Iterable, Iterator, Mapping, Sized
 from typing import Any
 
@@ -339,35 +338,3 @@ def get_class_weights(dataset: datasets.DatasetDict) -> torch.Tensor:
     weights = torch.nn.functional.softmax(torch.Tensor(weights), dim=-1) + 1
 
     return weights
-
-
-def keep_only(keep: list[str], sample: dict, oos: bool) -> dict:
-    """
-    Return a new `sample` with only the labels specified in `keep`.
-    """
-    keep_regex = re.compile(
-        rf"[BI]-({'|'.join(keep)})" r"|(?<![^\/])O+(?![^\/])"
-    )
-    # keep_regex will match any label contained in `keep` plus any sequence
-    # of the letter O, as long as it is not part of another label.
-
-    sample["nerc_tags"] = [
-        "/".join(
-            map(
-                lambda label: (
-                    label if keep_regex.match(label) else replace(label, oos)
-                ),
-                tag.split("/"),
-            )
-        )
-        for tag in sample["nerc_tags"]
-    ]
-
-    return sample
-
-
-def replace(label: str, oos: bool) -> str:
-    if oos:
-        return label[:2] + "OOS"
-    else:
-        return "O"
